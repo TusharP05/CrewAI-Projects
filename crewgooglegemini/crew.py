96b69b15c1896d41131e6ccbf5c27a6b9c5a3ca7
Tushar Parakh <21BCS4197@CUCHD.IN>
2024-07-28 20:42:08 +0530

    markdown

--- crewgooglegemini/crew.py
+++ crewgooglegemini/crew.py
@@ -2,33 +2,10 @@ import json
 from crewai import Crew, Process
 from tasks import research_task, write_task, link_finder
 from agents import news_researcher, news_writer, linker
-import markdown
+
 from bs4 import BeautifulSoup
 
-def markdown_to_json(markdown_text):
-    # Convert Markdown to HTML
-    html = markdown.markdown(markdown_text)
-    
-    # Parse HTML
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    sections = {}
-    current_heading = None
-    current_content = []
-    
-    for element in soup.descendants:
-        if element.name == 'h2' or element.name == 'h3' or element.name == 'h4':
-            if current_heading:
-                sections[current_heading] = ''.join(str(x) for x in current_content).strip()
-            current_heading = element.get_text()
-            current_content = []
-        else:
-            current_content.append(element)
-    
-    if current_heading:
-        sections[current_heading] = ''.join(str(x) for x in current_content).strip()
-    
-    return sections
+
 
 # Forming the tech-focused crew with some enhanced configuration
 crew = Crew(
